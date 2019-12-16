cdef59b014de4f1c4dc519ed2d95c7572e4d67ad
Trais McAllister <tmcallister@affirma.com>
2019-12-16 15:44:45 -0800

    Added FixedMessage and Progress Bar

---
+++ Consoul/FixedMessage/FixedMessage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoulLibrary.FixedMessage
+{
+    public class FixedMessage
+    {
+        protected int _x, _y, _fw;
+        public int? MaxWidth { get; set; }
+
+        public FixedMessage()
+        {
+            _x = Console.CursorLeft;
+            _y = Console.CursorTop;
+            _fw = Console.BufferWidth;
+        }
+        public FixedMessage(int maxWidth) : base()
+        {
+            MaxWidth = maxWidth;
+        }
+
+        public void Update(string message, ConsoleColor? color = null)
+        {
+            int prevX, prevY;
+            prevX = Console.CursorLeft;
+            prevY = Console.CursorTop;
+            Console.SetCursorPosition(_x, _y);
+            if (MaxWidth != null && message.Length > MaxWidth)
+                message = message.Substring(0, (int)MaxWidth - 3) + "...";
+            Consoul.Write(message, color, false);
+            Console.SetCursorPosition(prevX, prevY);
+        }
+    }
+}
---
+++ Consoul/Progress/ProgressBar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoulLibrary.Progress
+{
+    public class ProgressBar
+    {
+        private FixedMessage.FixedMessage _message { get; set; }
+        private FixedMessage.FixedMessage _bar { get; set; }
+        private int _total;
+        public int Total => _total;
+        public Char BlockCharacter { get; set; }
+
+        public ProgressBar(int total)
+        {
+            _total = total;
+            BlockCharacter = (char)0x2588;
+            _message = new FixedMessage.FixedMessage();
+            _message.Update(string.Empty);
+            Console.WriteLine();// Create Line Break
+            _bar = new FixedMessage.FixedMessage();
+            _bar.Update(string.Empty);
+            Console.WriteLine();// Create Line Break;
+        }
+
+        public void Start()
+        {
+            Consoul.Write(string.Empty);
+        }
+
+        public void Update(int index, string message = null, ConsoleColor? color = null)
+        {
+            Update((double)index / (double)_total, message, color);
+        }
+        public void Update(double percent, string message = null, ConsoleColor? color = null)
+        {
+            if (color == null)
+                color = ConsoleColor.Green;
+            int width = (int)(percent * (double)Console.BufferWidth);
+
+            _message.Update(message, RenderOptions.SubnoteColor);
+            _bar.Update(new string(BlockCharacter, width), color);
+        }
+    }
+}
