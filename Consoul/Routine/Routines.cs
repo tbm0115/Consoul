0b6caae2cb9aaebd015f59092bc5d0190bbd766b
tbm0115 <tbm0115@gmail.com>
2019-12-18 18:24:14 -0800

    Added startup routine to load XmlRoutine

--- Consoul/Routine/Routines.cs
+++ Consoul/Routine/Routines.cs
@@ -12,12 +12,25 @@ namespace ConsoulLibrary {
         public static void InitializeRoutine(string[] args)
         {
             int idxRoutineFlag = args.ToList().IndexOf("-Routine");
-            if (idxRoutineFlag < 0)
+            if (idxRoutineFlag >= 0 && (idxRoutineFlag + 1) > args.Length - 1) {
+                _loadRoutine(args[idxRoutineFlag + 1]);
                 return; // Continue without error
+            }
 
-            if ((idxRoutineFlag + 1) > args.Length - 1)
-                throw new IndexOutOfRangeException();
+            int idxXmlRoutineFlag = args.ToList().IndexOf("-XmlRoutine");
+            if (idxXmlRoutineFlag >= 0 && (idxXmlRoutineFlag + 1) > args.Length - 1) {
+                _loadXmlRoutine(args[idxXmlRoutineFlag + 1]);
+                return;
+            }
 
+        }
+        private static void _loadXmlRoutine(string filepath){
+            if (!System.IO.File.Exists(filepath))
+                return; // Continue without error
+            XmlRoutine xRoutine = new XmlRoutine(filepath);
+            Routines.InitializeRoutine(xRoutine);
+        }
+        private static void _loadRoutine(string assemblyName) {
             Assembly assembly = Assembly.GetCallingAssembly();
 
             Type[] allTypes = assembly.GetTypes();
@@ -26,14 +39,13 @@ namespace ConsoulLibrary {
             if (!routineTypes.Any())
                 throw new NotImplementedException();
 
-            Type routineType = routineTypes.FirstOrDefault(o => o.Name.Equals(args[idxRoutineFlag + 1], StringComparison.OrdinalIgnoreCase));
+            Type routineType = routineTypes.FirstOrDefault(o => o.Name.Equals(assemblyName, StringComparison.OrdinalIgnoreCase));
             if (routineType == null)
                 throw new KeyNotFoundException();
 
             Routine routine = routineType.GetConstructor(new Type[0]).Invoke(new object[0]) as Routine;
             if (routine == null)
                 throw new TypeLoadException();
-
             InitializeRoutine(routine);
         }
 
