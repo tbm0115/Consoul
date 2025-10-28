4aaaa3390877fbeba908762dbc3f49b882725220
Trais McAllister <tbm0115@gmail.com>
2025-10-27 22:45:07 -0400

    Default table selection to none (#40)
    * Default table selection to none
    
    * Separate hovered and selected table rows
    
    * Update TableView.cs

--- Consoul/Table/TableNavigator.cs
+++ Consoul/Table/TableNavigator.cs
@@ -8,6 +8,7 @@ namespace ConsoulLibrary
     /// </summary>
     public class TableNavigator
     {
+        private int _hoveredRow = -1;
         private int _currentRow = -1;
         private readonly int _totalRows;
         private readonly HashSet<int> _selectedRows;
@@ -18,36 +19,103 @@ namespace ConsoulLibrary
             _selectedRows = new HashSet<int>();
         }
 
+        public int HoveredRow => _hoveredRow;
         public int CurrentRow => _currentRow;
-        public int PreviousRow { get; private set; }
+        public int PreviousRow { get; private set; } = -1;
         public IReadOnlyCollection<int> SelectedRows => _selectedRows;
 
         public void MoveUp()
         {
-            PreviousRow = _currentRow;
-            _currentRow = Math.Max(0, _currentRow - 1);
+            PreviousRow = _hoveredRow;
+
+            if (_totalRows <= 0)
+            {
+                _hoveredRow = -1;
+                return;
+            }
+
+            if (_hoveredRow <= 0)
+            {
+                _hoveredRow = 0;
+                return;
+            }
+
+            _hoveredRow--;
         }
 
         public void MoveDown()
         {
-            PreviousRow = _currentRow;
-            _currentRow = Math.Min(_totalRows-1, _currentRow + 1);
+            PreviousRow = _hoveredRow;
+
+            if (_totalRows <= 0)
+            {
+                _hoveredRow = -1;
+                return;
+            }
+
+            if (_hoveredRow < 0)
+            {
+                _hoveredRow = 0;
+                return;
+            }
+
+            _hoveredRow = Math.Min(_totalRows - 1, _hoveredRow + 1);
         }
 
         public void ToggleSelection()
         {
-            if (_selectedRows.Contains(_currentRow))
-                _selectedRows.Remove(_currentRow);
+            if (_hoveredRow < 0)
+            {
+                return;
+            }
+
+            if (_selectedRows.Contains(_hoveredRow))
+            {
+                _selectedRows.Remove(_hoveredRow);
+
+                if (_currentRow == _hoveredRow)
+                {
+                    _currentRow = _selectedRows.Count > 0 ? GetFirstSelectedRow() : -1;
+                }
+            }
             else
-                _selectedRows.Add(_currentRow);
+            {
+                _selectedRows.Add(_hoveredRow);
+                _currentRow = _hoveredRow;
+            }
         }
 
         public void SetCurrentRow(int rowNumber)
         {
-            if (rowNumber < 0 || rowNumber > _totalRows-1) throw new ArgumentOutOfRangeException(nameof(rowNumber));
+            if (rowNumber < -1 || rowNumber > _totalRows - 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rowNumber));
+            }
 
-            PreviousRow = _currentRow;
+            PreviousRow = _hoveredRow;
+            _hoveredRow = rowNumber;
             _currentRow = rowNumber;
+            _selectedRows.Clear();
+
+            if (rowNumber >= 0)
+            {
+                _selectedRows.Add(rowNumber);
+            }
+        }
+
+        private int GetFirstSelectedRow()
+        {
+            int smallest = int.MaxValue;
+
+            foreach (var row in _selectedRows)
+            {
+                if (row < smallest)
+                {
+                    smallest = row;
+                }
+            }
+
+            return smallest == int.MaxValue ? -1 : smallest;
         }
     }
 }
